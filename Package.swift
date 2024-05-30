57b24f4dbc4e74fb4f3866d3a8b54daf1f2cd4dd
Roberto Casula <roberto@casula.dev>
2024-05-30 09:06:10 +0200

    Local swiftgen

--- Package.swift
+++ Package.swift
@@ -29,8 +29,7 @@ let package = Package(
     ),
     .binaryTarget(
       name: "swiftgen",
-      url: "https://github.com/SwiftGen/SwiftGen/releases/download/6.6.2/swiftgen-6.6.2.artifactbundle.zip",
-      checksum: "7586363e24edcf18c2da3ef90f379e9559c1453f48ef5e8fbc0b818fbbc3a045"
+      path: "bin/swiftgen-6.6.3.artifactbundle.zip"
     )
   ]
 )
